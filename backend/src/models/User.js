265795b2b148f4709064c5c951edf3b107a4e1cc
wangchao <wangchao8262@pansoft.com>
2026-07-02 12:42:20 +0800

    fix(backend): 修复 Mongoose pre-save 钩子 next 调用错误

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -52,18 +52,13 @@ const userSchema = new mongoose.Schema(
 );
 
 // 保存前加密密码
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // 验证密码方法
